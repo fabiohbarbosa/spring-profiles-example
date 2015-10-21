d767dfab17fe83924692428a5b4311a93317d4f1
Fabio H. G. Barbosa <fabiohbarbosa@gmail.com>
2015-10-21 08:26:38 -0200

    Removing XmlElement from POC entity

--- src/main/java/br/com/gsw/example/spring/profiles/poc/domain/Poc.java
+++ src/main/java/br/com/gsw/example/spring/profiles/poc/domain/Poc.java
@@ -1,24 +1,16 @@
 package br.com.gsw.example.spring.profiles.poc.domain;
 
 import javax.persistence.*;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
 import java.io.Serializable;
 
-@XmlRootElement(name = "poc")
-@XmlAccessorType(XmlAccessType.FIELD)
 @Entity(name = "poc")
 @SequenceGenerator(sequenceName = "POC_SEQUENCE", name = "POC_SEQUENCE", initialValue = 1, allocationSize = 1)
 public class Poc implements Serializable {
 
     @Id
     @GeneratedValue(generator = "POC_SEQUENCE", strategy = GenerationType.SEQUENCE)
-    @XmlElement
     private Long id;
 
-    @XmlElement
     private String name;
 
     public Poc() {
